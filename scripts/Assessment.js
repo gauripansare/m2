9428f90d864313e2e891c2b2849657dea847ba6d
atul0030 <atul@knowdl.com>
2019-02-01 11:51:46 +0530

    update files

--- scripts/Assessment.js
+++ scripts/Assessment.js
@@ -116,6 +116,9 @@ var _Assessment = (function () {
 				this.ShowQuestionPresenterMode();
 				$("#linknext").k_enable()
 			}
+			if (isFirefox || isIE11version){
+				this.AsseementFFCustomCheckboxAccessbility();
+			}
 			if (gRecordData.Questions[currentQuestionIndex].IsAnswered) {
 				this.ShowUserReviewMode();
 			}
@@ -154,6 +157,7 @@ var _Assessment = (function () {
 
 		},
 		ShowUserReviewMode: function () {
+			$(".assessmentSubmit").hide();
 			var currQuestion = gRecordData.Questions[currentQuestionIndex];
 			var correctoption = currQuestion.Options.filter(function (item) {
 				return item.IsCorrect;
@@ -187,8 +191,8 @@ var _Assessment = (function () {
 			$("input[type='radio']").k_disable();
 
 			if (isIE11version) {
-				$("input[type='radio']").removeAttr("aria-disabled");
-				this.SetCustomarialabelforRadio();
+				//$("input[type='radio']").removeAttr("aria-disabled");
+				//ATUL this.SetCustomarialabelforRadio();
 				//$("input[type='radio']").removeAttr("disabled")
 			}
 			$(".assessmentSubmit").hide();
@@ -209,6 +213,9 @@ var _Assessment = (function () {
 					optionObj = $(".Option").clone();
 					optionObj.find("input").attr("id", "question" + gRecordData.Questions[b].QuestionId + currQustion.Options[i].OptionId)
 					optionObj.find("input").attr("name", "radiobutton"+gRecordData.Questions[b].QuestionId )
+					if(isFirefox){
+						optionObj.find("input").attr("aria-label", currQustion.Options[i].OptionText)
+					}
 					optionObj.find(".inpputtext .ltext").html(currQustion.Options[i].OptionText)
 					optionObj.find(".inpputtext").attr("for", "question" + gRecordData.Questions[b].QuestionId + currQustion.Options[i].OptionId)
 					optionObj.removeClass("Option");
@@ -369,7 +376,18 @@ var _Assessment = (function () {
 					}
 				}
 			}
-		}
+		},
+		AsseementFFCustomCheckboxAccessbility: function () {            
+			var radioboxarray = $("input[type='radio']").map(function () {
+				return $(this).attr("id");
+			}).get();
+			for (var i = 0; i < radioboxarray.length; i++) {
+				var aria_label = $("label[for='" + radioboxarray[i] + "'] .ltext").html();
+				$("label[for='" + radioboxarray[i] + "'] ").attr("aria-hidden", "true");
+				$("#" + radioboxarray[i]).attr("aria-label", aria_label);
+
+			}
+	}
 
 	}
 })();
